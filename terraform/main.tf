018be90d1de7a6e5469673e181ec3fcbc474c41e
karthikeyan <karthikeyan@bicsglobal.com>
2025-03-19 11:55:55 +0530

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_lambda_layer_version" "lambda_layer" {
   layer_name          = var.layer_name
   compatible_runtimes = ["python3.8", "python3.9", "python3.10", "python3.12"]
