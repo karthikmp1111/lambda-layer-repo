72b076b2b529dae102bae8d7bcacf6b4d7ce2129
karthikeyan <karthikeyan@bicsglobal.com>
2025-03-19 12:56:04 +0530

    modified path

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   layer_name          = var.layer_name
   compatible_runtimes = ["python3.8", "python3.9", "python3.10", "python3.12"]
   # filename            = "../lambda_layer.zip"
-  filename            = "./lambda_layer.zip"
+  filename            = "../lambda-layer/lambda_layer.zip"
 }
 
 output "layer_arn" {
